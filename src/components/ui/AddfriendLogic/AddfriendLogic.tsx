94cfe9ffb0fd2d695af0b8599408d777efa6f760
siam <samisiam851@gmail.com>
2024-02-01 17:41:18 +0600

    add friend input form done

--- src/components/ui/AddfriendLogic/AddfriendLogic.tsx
+++ src/components/ui/AddfriendLogic/AddfriendLogic.tsx
@@ -1,12 +1,18 @@
 'use client'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 type Props = {}
+type inputObject = {
 
+}
 const AddfriendLogic = (props: Props) => {
 
+    const { register, handleSubmit } = useForm()
+    const addFriend = async (data: object) => {
 
-    const addFriend = async (email: string) => {
+        console.log(data.input);
+
         try {
 
         } catch (error) {
@@ -16,12 +22,12 @@ const AddfriendLogic = (props: Props) => {
 
     return (
         <div>
-            <form className='max-w-sm border p-2' action="">
+            <form className='max-w-sm border p-2' onClick={handleSubmit(addFriend)}>
                 <label className='' htmlFor='email'>Add friend by E-mail</label>
                 <div className="flex py-5 space-x-4">
-                    <input
-                        type="email"
-                        placeholder="example: you@email.com"
+                    <input {...register('input')}
+                        type="text"
+                        placeholder="you@email.com or name"
                         className="px-4 py-0 border border-gray-300 text-gray-600 rounded-md focus:outline-none focus:border-indigo-500"
                     />
                     <button className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:bg-indigo-600 focus:outline-none">
